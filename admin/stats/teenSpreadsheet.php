92b05e345ec5f4944939cffd6a6298c63a07554c
Tim Peterson <tim.peterson@kentonlibrary.org>
2019-06-11 12:58:12 -0400

    Updates teen spreadsheet to use startDate

--- admin/stats/teenSpreadsheet.php
+++ admin/stats/teenSpreadsheet.php
@@ -1,10 +1,10 @@
 <?php
 include('../../assets/scripts.php'); //File with connection information and functions
-
+include('../../assets/settings.php');
 // output headers so that the file is downloaded rather than displayed
 header('Content-type: text/csv');
 $filename = "TEEN-SRC " . date("Y-m-d h:ma");
-header('Content-Disposition: attachment; filename="' . $filename . '"');
+header('Content-Disposition: attachment; filename="' . $filename . '.csv"');
  
 // do not cache the file
 header('Pragma: no-cache');
@@ -17,7 +17,7 @@ $file = fopen('php://output', 'w');
 fputcsv($file, array('First Name', 'Last Name', 'Birthday', 'Grade', 'School', 'Branch', 'Phone Number', 'Email', 'Book Read', 'Rating', 'Timestamp'));
 
 
-$query = "SELECT reader.readerFirstName, reader.readerLastName, reader.readerbirthDate, reader.readerGrade, reader.readerSchool, account.branch, account.phoneNumber, account.emailAddress, teenLog.title, teenLog.rating, teenLog.timeStamp FROM reader, account, teenLog WHERE reader.accountID = account.accountID AND reader.readerID = teenLog.readerID AND reader.readerCategory = 'teen'";
+$query = "SELECT reader.readerFirstName, reader.readerLastName, reader.readerbirthDate, reader.readerGrade, reader.readerSchool, account.branch, account.phoneNumber, account.emailAddress, teenLog.title, teenLog.rating, teenLog.timeStamp FROM reader, account, teenLog WHERE reader.accountID = account.accountID AND reader.readerID = teenLog.readerID AND reader.readerCategory = 'teen' AND DATE(teenLog.timestamp) >= '$startDate'";
 
 if($rows = mysqli_query($connection, $query)){
   //loop over the rows, outputting them
